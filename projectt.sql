2cd86453620434302b83a93686bcb027e08737a6
WH <a187796417@163.com>
2025-03-20 11:07:21 +0800

    存储数据库

---
+++ projectt.sql
@@ -0,0 +1,334 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : wh
+ Source Server Type    : MySQL
+ Source Server Version : 80039 (8.0.39)
+ Source Host           : localhost:3306
+ Source Schema         : projectt
+
+ Target Server Type    : MySQL
+ Target Server Version : 80039 (8.0.39)
+ File Encoding         : 65001
+
+ Date: 20/03/2025 11:05:55
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for carpark
+-- ----------------------------
+DROP TABLE IF EXISTS `carpark`;
+CREATE TABLE `carpark`  (
+  `index` int NOT NULL AUTO_INCREMENT,
+  `LicNumber` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '车牌号',
+  `StartParkTime` datetime NOT NULL,
+  `EndParkTime` datetime NULL DEFAULT NULL,
+  `ParkLocal` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '停放位置 x/y',
+  PRIMARY KEY (`index`) USING BTREE,
+  INDEX `LicNumber`(`LicNumber` ASC) USING BTREE,
+  CONSTRAINT `LicNumber` FOREIGN KEY (`LicNumber`) REFERENCES `cars` (`CarNumber`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of carpark
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cars
+-- ----------------------------
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars`  (
+  `CarType` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '车型',
+  `CarID` int NOT NULL AUTO_INCREMENT COMMENT '车辆编号',
+  `CarNumber` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `userid` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`CarID`) USING BTREE,
+  INDEX `userid`(`userid` ASC) USING BTREE,
+  INDEX `CarNumber`(`CarNumber` ASC) USING BTREE,
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cars
+-- ----------------------------
+INSERT INTO `cars` VALUES ('黑色宝马', 1, '鲁B32575', '004');
+INSERT INTO `cars` VALUES ('蓝色奔驰', 2, '粤A65357', '004');
+INSERT INTO `cars` VALUES ('我的爱马', 3, '京A66666', '004');
+INSERT INTO `cars` VALUES ('我的爱马', 4, '京A00461', '005');
+
+-- ----------------------------
+-- Table structure for dict
+-- ----------------------------
+DROP TABLE IF EXISTS `dict`;
+CREATE TABLE `dict`  (
+  `id` bigint NOT NULL DEFAULT 0 COMMENT 'id',
+  `parent_id` bigint NOT NULL DEFAULT 0 COMMENT '上级id',
+  `name` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '' COMMENT '名称',
+  `value` bigint NULL DEFAULT NULL COMMENT '值',
+  `dict_code` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '编码',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '删除标记（0:未删除 1:已删除）',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_dict_code`(`dict_code` ASC) USING BTREE,
+  INDEX `idx_parent_id`(`parent_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '组织架构表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dict
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for hospital_set
+-- ----------------------------
+DROP TABLE IF EXISTS `hospital_set`;
+CREATE TABLE `hospital_set`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `hosname` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '医院名称',
+  `hoscode` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '医院编号',
+  `api_url` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT 'api基础路径',
+  `sign_key` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '签名秘钥',
+  `contacts_name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人',
+  `contacts_phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人手机',
+  `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_hoscode`(`hoscode` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '医院设置表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of hospital_set
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `# USEr_id` bigint NULL DEFAULT NULL,
+  `out_trade_no` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '订单交易号',
+  `hoscode` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '医院编号',
+  `hosname` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '医院名称',
+  `depcode` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '科室编号',
+  `depname` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '科室名称',
+  `title` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '医生职称',
+  `hos_schedule_id` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '排班编号（医院自己的排班主键）',
+  `reserve_date` date NULL DEFAULT NULL COMMENT '安排日期',
+  `reserve_time` tinyint NULL DEFAULT 0 COMMENT '安排时间（0：上午 1：下午）',
+  `patient_id` bigint NULL DEFAULT NULL COMMENT '就诊人id',
+  `patient_name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '就诊人名称',
+  `patient_phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '就诊人手机',
+  `hos_record_id` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '预约记录唯一标识（医院预约记录主键）',
+  `number` int NULL DEFAULT NULL COMMENT '预约号序',
+  `fetch_time` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '建议取号时间',
+  `fetch_address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '取号地点',
+  `amount` decimal(10, 0) NULL DEFAULT NULL COMMENT '医事服务费',
+  `quit_time` datetime NULL DEFAULT NULL COMMENT '退号时间',
+  `order_status` tinyint NULL DEFAULT NULL COMMENT '订单状态',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_out_trade_no`(`out_trade_no` ASC) USING BTREE,
+  INDEX `idx_# USEr_id`(`# USEr_id` ASC) USING BTREE,
+  INDEX `idx_hoscode`(`hoscode` ASC) USING BTREE,
+  INDEX `idx_hos_schedule_id`(`hos_schedule_id` ASC) USING BTREE,
+  INDEX `idx_hos_record_id`(`hos_record_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of order_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for park
+-- ----------------------------
+DROP TABLE IF EXISTS `park`;
+CREATE TABLE `park`  (
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '位置',
+  `parkID` int NOT NULL COMMENT '编号',
+  `maxParking` int NULL DEFAULT NULL COMMENT '车位数量',
+  `nowParking` int NULL DEFAULT NULL COMMENT '当前停放车位',
+  `manageID` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '管理员账号',
+  `opening` tinyint(1) NULL DEFAULT NULL COMMENT '是否开放',
+  `PosX` double(255, 10) NULL DEFAULT NULL COMMENT '纬度',
+  `PosY` double(255, 10) NULL DEFAULT NULL COMMENT '经度',
+  PRIMARY KEY (`parkID`) USING BTREE,
+  INDEX `manageID`(`manageID` ASC) USING BTREE,
+  CONSTRAINT `manageID` FOREIGN KEY (`manageID`) REFERENCES `users` (`user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of park
+-- ----------------------------
+INSERT INTO `park` VALUES ('命莲寺地下', 1, 1000, 1, '002', 1, 32.0433360000, 120.8087170000);
+INSERT INTO `park` VALUES ('阳光赛马场', 2, 2000, 0, '002', 0, 32.0195748161, 120.8588790894);
+INSERT INTO `park` VALUES ('南', 3, 3000, 0, '002', 0, 32.0246687851, 120.9132957458);
+
+-- ----------------------------
+-- Table structure for patient
+-- ----------------------------
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE `patient`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `# USEr_id` bigint NULL DEFAULT NULL COMMENT '用户id',
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `certificates_type` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '证件类型',
+  `certificates_no` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '证件编号',
+  `sex` tinyint NULL DEFAULT NULL COMMENT '性别',
+  `birthdate` date NULL DEFAULT NULL COMMENT '出生年月',
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '手机',
+  `is_marry` tinyint NULL DEFAULT NULL COMMENT '是否结婚',
+  `province_code` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '省code',
+  `city_code` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '市code',
+  `district_code` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '区code',
+  `address` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '详情地址',
+  `contacts_name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人姓名',
+  `contacts_certificates_type` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人证件类型',
+  `contacts_certificates_no` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人证件号',
+  `contacts_phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '联系人手机',
+  `card_no` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '就诊卡号',
+  `is_insure` tinyint NULL DEFAULT 0 COMMENT '是否有医保',
+  `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态（0：默认 1：已认证）',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_# USEr_id`(`# USEr_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '就诊人表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of patient
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for payment_info
+-- ----------------------------
+DROP TABLE IF EXISTS `payment_info`;
+CREATE TABLE `payment_info`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `out_trade_no` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '对外业务编号',
+  `order_id` bigint NULL DEFAULT NULL COMMENT '订单id',
+  `payment_type` tinyint(1) NULL DEFAULT NULL COMMENT '支付类型（微信 支付宝）',
+  `trade_no` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '交易编号',
+  `total_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '支付金额',
+  `subject` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '交易内容',
+  `payment_status` tinyint NULL DEFAULT NULL COMMENT '支付状态',
+  `callback_time` datetime NULL DEFAULT NULL COMMENT '回调时间',
+  `callback_content` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '回调信息',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_out_trade_no`(`out_trade_no` ASC) USING BTREE,
+  INDEX `idx_order_id`(`order_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '支付信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of payment_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for refund_info
+-- ----------------------------
+DROP TABLE IF EXISTS `refund_info`;
+CREATE TABLE `refund_info`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `out_trade_no` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '对外业务编号',
+  `order_id` bigint NULL DEFAULT NULL COMMENT '订单编号',
+  `payment_type` tinyint NULL DEFAULT NULL COMMENT '支付类型（微信 支付宝）',
+  `trade_no` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '交易编号',
+  `total_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '退款金额',
+  `subject` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '交易内容',
+  `refund_status` tinyint NULL DEFAULT NULL COMMENT '退款状态',
+  `callback_content` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '回调信息',
+  `callback_time` datetime NULL DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_out_trade_no`(`out_trade_no` ASC) USING BTREE,
+  INDEX `idx_order_id`(`order_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '退款信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of refund_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `openid` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '微信openid',
+  `nick_name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '用户姓名',
+  `certificates_type` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '证件类型',
+  `certificates_no` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '证件编号',
+  `certificates_url` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '证件路径',
+  `auth_status` tinyint NOT NULL DEFAULT 0 COMMENT '认证状态（0：未认证 1：认证中 2：认证成功 -1：认证失败）',
+  `status` tinyint NOT NULL DEFAULT 1 COMMENT '状态（0：锁定 1：正常）',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `uk_mobile`(`phone` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_login_record
+-- ----------------------------
+DROP TABLE IF EXISTS `user_login_record`;
+CREATE TABLE `user_login_record`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `# USEr_id` bigint NULL DEFAULT NULL COMMENT '用户id',
+  `ip` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT 'ip',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_# USEr_id`(`# USEr_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 40 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户登录记录表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_login_record
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `passcode` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `telnum` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '姓名',
+  `identity` int NOT NULL COMMENT '身份类型',
+  `sub` int NOT NULL AUTO_INCREMENT COMMENT '序号',
+  `user` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账号\r\n',
+  `qqID` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'qq号',
+  PRIMARY KEY (`sub` DESC) USING BTREE,
+  INDEX `user`(`user` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('999', '18013980699', '孙瑞阳', 0, 5, '005', '1790789430');
+INSERT INTO `users` VALUES ('000', '13663532518', '李帅', 0, 4, '004', '3013629683');
+INSERT INTO `users` VALUES ('003', '1234567', '王五', 2, 3, '003', '1008611');
+INSERT INTO `users` VALUES ('002', '654321', '李四', 1, 2, '002', NULL);
+INSERT INTO `users` VALUES ('001', '123456', '张三', 0, 1, '001', '3555617336');
+
+SET FOREIGN_KEY_CHECKS = 1;
